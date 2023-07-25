28ad5f66c76446065d9d8bdd82aca3c5d1761010
Rodrigo Alejandro Mora Azocar <brazuk25@gmail.com>
2023-07-25 20:12:36 -0300

    Alteraciones - Trsnspilacion - tsconfig

---
+++ ts-demo/build/index.js
@@ -0,0 +1,9 @@
+"use strict";
+console.log("Hola Mundo! Este es mi primer comando TypeScript");
+//Esto es un comentario en TypeScript
+/**
+ *
+ * Otro tipo de comentario - para multilinea
+ *
+ *
+ *  */ 
--- ts-demo/src/index.ts
+++ ts-demo/src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hola TypeScript");
+console.log("Hola Mundo! Este es mi primer comando TypeScript");
+
+//Esto es un comentario en TypeScript
+
+
+/**
+ * 
+ * Otro tipo de comentario - para multilinea
+ * 
+ * 
+ *  */
